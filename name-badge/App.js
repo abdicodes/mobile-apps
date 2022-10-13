3566ce58a10fe733aa335522c6783b56f8c09680
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-10-13 22:30:25 +0300

    welcomeText style

--- name-badge/App.js
+++ name-badge/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function App() {
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +15,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f00',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcomeText: {
+    fontSize: 90,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
   },
 });
