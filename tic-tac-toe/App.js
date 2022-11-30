b846244b8d316d068bc631ea1ef9bd6766f1bca5
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-11-30 04:47:19 +0200

    implemented borders

--- tic-tac-toe/App.js
+++ tic-tac-toe/App.js
@@ -1,29 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        {' '}
-        <Text>X</Text>
-        <Text>O</Text>
-        <Text>X</Text>
-      </View>
+    <View style={styles.containerColumn}>
+      <SafeAreaView style={styles.containerColumn}>
+        <View style={[styles.containerRow, styles.horizontalLine]}>
+          <View style={styles.verticalLine}>
+            <Text style={styles.text}>X</Text>
+          </View>
 
-      <View>
-        {' '}
-        <Text>O</Text>
-        <Text>X</Text>
-        <Text>O</Text>
-      </View>
+          <View style={styles.verticalLine}>
+            <Text style={styles.text}>O</Text>
+          </View>
+          <View>
+            <Text style={styles.text}>X</Text>
+          </View>
+        </View>
 
-      <View>
-        {' '}
-        <Text>O</Text>
-        <Text>O</Text>
-        <Text>X</Text>
-      </View>
+        <View style={[styles.containerRow, styles.horizontalLine]}>
+          <View style={styles.verticalLine}>
+            <Text style={styles.text}>O</Text>
+          </View>
+          <View style={styles.verticalLine}>
+            <Text style={styles.text}>X</Text>
+          </View>
+          <View>
+            <Text style={styles.text}>O</Text>
+          </View>
+        </View>
+
+        <View style={styles.containerRow}>
+          <View style={styles.verticalLine}>
+            <Text style={styles.text}>O</Text>
+          </View>
+          <View style={styles.verticalLine}>
+            <Text style={styles.text}>O</Text>
+          </View>
+          <View>
+            <Text style={styles.text}>X</Text>
+          </View>
+        </View>
+      </SafeAreaView>
 
       <StatusBar style="auto" />
     </View>
@@ -31,14 +49,26 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerColumn: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerRow: {
     flexDirection: 'row',
   },
   text: {
-    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  horizontalLine: {
+    borderBottomWidth: 2,
+    borderBottomColor: 'black',
+  },
+
+  verticalLine: {
+    borderRightWidth: 2,
+    borderRightColor: 'black',
   },
 });
