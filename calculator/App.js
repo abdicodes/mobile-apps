6d498b940a0a6a2d7059d67dd06ff561338ef591
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-12-08 00:48:20 +0200

    set the status bar styling

--- calculator/App.js
+++ calculator/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +23,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  text: {
+    color: 'white',
   },
 });
