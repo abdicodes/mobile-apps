030f96f3a0108a6de7831b2989066e77fce247d1
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-12-08 02:37:58 +0200

    implemented UI partially 1st row buttons & result

--- calculator/App.js
+++ calculator/App.js
@@ -8,15 +8,31 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { useState } from 'react';
+const { width } = Dimensions.get('window');
 
+const ButtonWidth = width / 5;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.result}>
+        <Text style={styles.resultText}>32</Text>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={[styles.Buttons, styles.lightGrey]}>
+          <Text style={styles.blackButtonText}>C</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.Buttons, styles.lightGrey]}>
+          <Text style={styles.blackButtonText}>+/-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.Buttons, styles.lightGrey]}>
+          <Text style={styles.blackButtonText}>%</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.Buttons, styles.lightGrey]}>
+          <Text style={styles.blackButtonText}>/</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="light" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -24,10 +40,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-    alignItems: 'center',
     justifyContent: 'flex-end',
   },
-  text: {
-    color: 'white',
+  resultText: {
+    color: '#fff',
+    fontSize: ButtonWidth,
+  },
+
+  blackButtonText: {
+    color: '#000',
+    fontSize: ButtonWidth / 2,
+  },
+  result: {
+    alignItems: 'flex-end',
+  },
+
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  Buttons: {
+    width: ButtonWidth,
+    height: ButtonWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: ButtonWidth,
+  },
+  lightGrey: {
+    backgroundColor: '#C0C0C0',
+  },
+  blueButtons: {
+    backgroundColor: '#00BFFF',
+  },
+  darkGreyButtons: {
+    backgroundColor: '#808080',
   },
 });
