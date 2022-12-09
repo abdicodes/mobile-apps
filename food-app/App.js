e069e49cff127e70901bf9fb98260dcbf0ad476d
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-12-09 02:15:21 +0200

    UI basics constructed partially

--- food-app/App.js
+++ food-app/App.js
@@ -1,17 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Cell, Section, TableView } from 'react-native-tableview-simple';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+const HomescreenCell = (props) => {
+  return <Cell {...props} />;
+};
 const Stack = createStackNavigator();
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <TableView>
+          <Section name="" hideSeparator="true" separatorTintColor="#ccc">
+            <HomescreenCell />
+          </Section>
+        </TableView>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Restaurants" component={null} />
-        <Stack.Screen name="Menu" component={null} />
+        <Stack.Screen name="Restaurants" component={Home} />
+        {/* <Stack.Screen name="Menu" component={null} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
